18f3615e3aedbd8d07c346e45a46b9fad46f67dc
senthilkumar63 <127287380+senthilkumar63@users.noreply.github.com>
2023-04-14 15:28:24 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Allcontent from "../Components/Allcontent";
+import App from "../DropDown/App";
+
+function HomePage() {
+    return <>
+        <div> 
+            <App />       
+            <Allcontent />
+        </div></>
+}
+
+export default HomePage;
+
